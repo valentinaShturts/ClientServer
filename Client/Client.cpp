49bed41247b24c5600488dc253a88ef17f363711
valentinaShturts <valentinashturts@gmail.com>
2025-02-26 14:29:50 +0200

    talk

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -7,7 +7,6 @@
 #include <ws2tcpip.h>
 using namespace std;
 
-// потрібно зв'язати з Ws2_32.lib, Mswsock.lib та Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
@@ -17,37 +16,15 @@ using namespace std;
 
 #define PAUSE 0
 
-int main(int argc, char** argv) // ім'я сервера при бажанні можна буде вказати через параметри командного рядка
+int main(int argc, char** argv) 
 {
-    // я намагався максимально спростити запуск клієнтського додатку, тому кількість параметрів командного рядка не перевіряється!
-    // перевірка параметрів якщо треба
-    // if (argc != 2) {
-    //    printf("usage: %s server-name\n", argv[0]);
-    //    return 10;
-    // }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    setlocale(0, "Ukrainian");
     system("title CLIENT SIDE");
-    //cout << "the client process is started!\n";
-    Sleep(PAUSE);
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // iніціалізація Winsock
     WSADATA wsaData;
     int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
-        cout << "WSAStartup failed with error: " << iResult << "\n";
         return 11;
     }
-    else {
-        //cout << "connection to Winsock.dll was successful!\n";
-        Sleep(PAUSE);
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     addrinfo hints;
     ZeroMemory(&hints, sizeof(hints));
@@ -55,40 +32,27 @@ int main(int argc, char** argv) // ім'я сервера при бажанні 
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
-    // отримання адреси сервера та порту
-    const char* ip = "localhost"; // за замовчуванням, обидва додатки, і клієнт, і сервер, працюють на одній і тій самій машині
+    const char* ip = "localhost"; 
 
-    // iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result); // раскоментувати, якщо потрібно буде зчитувати ім'я сервера з командного рядка
     addrinfo* result = NULL;
     iResult = getaddrinfo(ip, DEFAULT_PORT, &hints, &result);
 
     if (iResult != 0) {
-        cout << "getaddrinfo failed with error: " << iResult << "\n";
         WSACleanup();
         return 12;
     }
-    else {
-        //cout << "obtaining the client address and port was successful!\n";
-        Sleep(PAUSE);
-    }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // спроба підключення до адреси до успіху
     SOCKET ConnectSocket = INVALID_SOCKET;
 
-    for (addrinfo* ptr = result; ptr != NULL; ptr = ptr->ai_next) { // серверів може бути кілька, тому не завадить цикл
+    for (addrinfo* ptr = result; ptr != NULL; ptr = ptr->ai_next) { 
 
-        // створення SOCKET для підключення до сервера
         ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
 
         if (ConnectSocket == INVALID_SOCKET) {
-            cout << "socket failed to create with error: " << WSAGetLastError() << "\n";
             WSACleanup();
             return 13;
         }
 
-        // підключення до сервера
         iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
         if (iResult == SOCKET_ERROR) {
             closesocket(ConnectSocket);
@@ -96,13 +60,8 @@ int main(int argc, char** argv) // ім'я сервера при бажанні 
             continue;
         }
 
-        //cout << "creation of a socket on the client was successful!\n";
-        Sleep(PAUSE);
-
         break;
     }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     freeaddrinfo(result);
 
@@ -113,64 +72,43 @@ int main(int argc, char** argv) // ім'я сервера при бажанні 
     }
     else {
         cout << "connection to the server was successful!\n";
-        Sleep(PAUSE);
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // відправка початкового буфера
-    char message[DEFAULT_BUFLEN];
-    cout << "Enter message: ";
-    cin.getline(message, DEFAULT_BUFLEN);
-    iResult = send(ConnectSocket, message, strlen(message), 0);
-    if (iResult == SOCKET_ERROR) {
-        cout << "send failed with error: " << WSAGetLastError() << "\n";
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 15;
-    }
-    else {
-        cout << "data has been successfully sent to the server: " << message << "\n";
-        //cout << "bytes sent from the client: " << iResult << "\n";
-        Sleep(PAUSE);
-    }
+    while (true)
+    {
+        char message[DEFAULT_BUFLEN];
+        cout << "Enter message: ";
+        cin.getline(message, DEFAULT_BUFLEN);
+        iResult = send(ConnectSocket, message, strlen(message), 0);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            WSACleanup();
+            return 15;
+        }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // закриття з'єднання, оскільки більше даних не буде відправлено
-    iResult = shutdown(ConnectSocket, SD_SEND);
-    if (iResult == SOCKET_ERROR) {
-        cout << "shutdown failed with error: " << WSAGetLastError() << "\n";
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 16;
-    }
-    else {
-        //cout << "the client process initiates closing the connection with the server.\n";
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // отримання даних до закриття з'єднання зі сторони сервера
-    char answer[DEFAULT_BUFLEN];
-
-    do {
+        char answer[DEFAULT_BUFLEN];
 
         iResult = recv(ConnectSocket, answer, DEFAULT_BUFLEN, 0);
         answer[iResult] = '\0';
 
         if (iResult > 0) {
             cout << "the server process sent a response: " << answer << "\n";
-            //cout << "bytes received: " << iResult << "\n";
         }
         else if (iResult == 0)
             cout << "the connection to the server is closed.\n";
-        else
-            cout << "recv failed with error: " << WSAGetLastError() << "\n";
 
-    } while (iResult > 0);
+        if (strcmp(answer, "bye, client!") == 0) break;
+        
+    }
 
-    // очистка
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 16;
+    }
+
     closesocket(ConnectSocket);
     WSACleanup();
 
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,130 +1,74 @@
-﻿// якщо у вас мак, тоді буде щось накшталт: https://gist.github.com/sunmeat/830e8cb963ff95c76498c67e3714b300
-
-// https://docs.microsoft.com/en-us/windows/win32/winsock/complete-server-code
-// #undef UNICODE
-
-#define WIN32_LEAN_AND_MEAN // для прискорення процесу збірки: https://stackoverflow.com/questions/11040133/what-does-defining-win32-lean-and-mean-exclude-exactly
+﻿#define WIN32_LEAN_AND_MEAN 
 
 #include <iostream>
 #include <windows.h>
 // #include <winsock2.h>
-#include <ws2tcpip.h> // тип WSADATA, функції WSAStartup, WSACleanup та багато чого іншого
+#include <ws2tcpip.h>
 using namespace std;
 
-// реалізація бібліотеки Winsock 2 DLL
 #pragma comment (lib, "Ws2_32.lib")
 // #pragma comment (lib, "Mswsock.lib")
 
 #define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "27015" // порт — це логічна конструкція, яка ідентифікує конкретний процес або тип мережевої служби - https://en.wikipedia.org/wiki/Port_(computer_networking)
-
+#define DEFAULT_PORT "27015" 
 #define PAUSE 0
 
 int main()
 {
-    setlocale(0, "Ukrainian");
     system("title SERVER SIDE");
-    //cout << "the server process is started!\n";
-    Sleep(PAUSE);
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    WSADATA wsaData; 
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); 
 
-    // ініціалізація Winsock
-    WSADATA wsaData; // Структура WSADATA містить інформацію про реалізацію Windows Sockets: https://docs.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-wsadata
-    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // Функція WSAStartup ініціює використання бібліотеки Winsock DLL процесом: https://firststeps.ru/mfc/net/socket/r.php?2
-    if (iResult != 0) {
-        cout << "WSAStartup failed with error: " << iResult << "\n";
-        cout << "Winsock.dll failed to connect!\n";
-        return 1;
-    }
-    else {
-        //cout << "Winsock.dll connection was successful!\n";
-        Sleep(PAUSE);
-    }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    struct addrinfo hints; // структура addrinfo використовується функцією getaddrinfo для зберігання інформації про адресу хоста: https://docs.microsoft.com/en-us/windows/win32/api/ws2def/ns-ws2def-addrinfoa
+    struct addrinfo hints; 
     ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET; // адреса сімейства протоколу Інтернету версії 4 (IPv4)
-    hints.ai_socktype = SOCK_STREAM; // забезпечує послідовні, надійні, двосторонні, засновані на з'єднанні потоки байтів з механізмом передачі даних
-    hints.ai_protocol = IPPROTO_TCP; // протокол передачі управління (TCP). Це можливе значення, коли член ai_family є AF_INET або AF_INET6, а член ai_socktype — SOCK_STREAM
-    hints.ai_flags = AI_PASSIVE; // адреса сокета буде використовуватися в виклику функції "bind"
+    hints.ai_family = AF_INET; 
+    hints.ai_socktype = SOCK_STREAM; 
+    hints.ai_protocol = IPPROTO_TCP; 
+    hints.ai_flags = AI_PASSIVE; 
 
-    // визначення адреси та порту сервера
+
     struct addrinfo* result = NULL;
     iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
     if (iResult != 0) {
-        cout << "getaddrinfo failed with error: " << iResult << "\n";
-        cout << "getting server address and port failed!\n";
-        WSACleanup(); // функція WSACleanup завершує використання бібліотеки Winsock 2 DLL (Ws2_32.dll): https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-wsacleanup
+        WSACleanup(); 
         return 2;
     }
-    else {
-        //cout << "receiving the server address and port was successful!\n";
-        Sleep(PAUSE);
-    }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // створення сокету для підключення до сервера
     SOCKET ListenSocket = INVALID_SOCKET;
     ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (ListenSocket == INVALID_SOCKET) {
-        cout << "socket failed to create with error: " << WSAGetLastError() << "\n";
-        cout << "socket creation failed!\n";
         freeaddrinfo(result);
         WSACleanup();
 
         return 3;
     }
-    else {
-        //cout << "creation of a socket on the server was successful!\n";
-        Sleep(PAUSE);
-    }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // налаштування сокета для прослуховування
-    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen); // Функція bind асоціює локальну адресу з сокетом: https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-bind
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen); 
     if (iResult == SOCKET_ERROR) {
-        cout << "bind failed with error: " << WSAGetLastError() << "\n";
-        cout << "the plug-in of the socket by IP address failed!\n";
         freeaddrinfo(result);
         closesocket(ListenSocket);
         WSACleanup();
         return 4;
     }
-    else {
-        //cout << "the implementation of the socket by IP address was successful!\n";
-        Sleep(PAUSE);
-    }
 
     freeaddrinfo(result);
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    iResult = listen(ListenSocket, SOMAXCONN); // функція listen ставить сокет у стан, коли він чекає вхідне підключення: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-listen
+    iResult = listen(ListenSocket, SOMAXCONN); 
     if (iResult == SOCKET_ERROR) {
-        cout << "listen failed with error: " << WSAGetLastError() << "\n";
-        cout << "listening to information from the client has not started. something went wrong!\n";
         closesocket(ListenSocket);
         WSACleanup();
         return 5;
     }
     else {
         cout << "listening to information from the client begins. please launch the client! (client.exe)\n";
-        // тут можна було б запустити якийсь прелоадер в окремому потоці
     }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // прийом сокета клієнта
     SOCKET ClientSocket = INVALID_SOCKET;
-    ClientSocket = accept(ListenSocket, NULL, NULL); // Функція accept дозволяє прийняти вхідне підключення на сокет: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-accept
+    ClientSocket = accept(ListenSocket, NULL, NULL); 
     if (ClientSocket == INVALID_SOCKET) {
-        cout << "accept failed with error: " << WSAGetLastError() << "\n";
-        cout << "connection with the client program is not established! sad, sad beyond measure ((\n";
         closesocket(ListenSocket);
         WSACleanup();
         return 6;
@@ -133,47 +77,38 @@ int main()
         cout << "the connection with the client program has been established successfully!\n";
     }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // більше не потрібно зберігати сокет сервера
     closesocket(ListenSocket);
 
-    // отримання даних до того моменту, поки з'єднання не буде завершено
     do {
 
         char message[DEFAULT_BUFLEN];
 
-        iResult = recv(ClientSocket, message, DEFAULT_BUFLEN, 0); // функція recv використовується для читання вхідних даних: https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-recv
+        iResult = recv(ClientSocket, message, DEFAULT_BUFLEN, 0); 
         message[iResult] = '\0';
 
         if (iResult > 0) {
             cout << "the client process sent a message: " << message << "\n";
-            Sleep(PAUSE);
-            //cout << "bytes received: " << iResult << "\n";
-            Sleep(PAUSE);
+            const char* answer;
 
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            // відправка відповіді назад відправнику
-            const char* answer = "and hello from server!";
+            if (strcmp(message, "hello") == 0) answer = "yo, client!";
+            else if (strcmp(message, "how is it going") == 0) answer = "fine, client!";
+            else if (strcmp(message, "is the pizza delicious?") == 0) answer = "sure is, client!";
+            else if (strcmp(message, "goodbye") == 0) answer = "bye, client!";
+            else
+            {
+                answer = "what are you talking about, client?";
+            }
             cout << "the server process sends a response: " << answer << "\n";
 
-            int iSendResult = send(ClientSocket, answer, strlen(answer), 0); // функція send відправляє дані на підключений сокет: https://docs.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-send
-
-            Sleep(PAUSE);
-
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            int iSendResult = send(ClientSocket, answer, strlen(answer), 0); 
 
             if (iSendResult == SOCKET_ERROR) {
-                cout << "send failed with error: " << WSAGetLastError() << "\n";
-                cout << "oops, sending (send) of the corresponding message did not take place ((\n";
                 closesocket(ClientSocket);
                 WSACleanup();
                 return 7;
             }
             else {
                 //cout << "bytes sent: " << iSendResult << "\n";
-                Sleep(PAUSE);
             }
         }
         else if (iResult == 0) {
@@ -190,13 +125,8 @@ int main()
 
     } while (iResult > 0);
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // закриття з'єднання, оскільки роботу завершено
-    iResult = shutdown(ClientSocket, SD_SEND); // функція shutdown вимикає надсилання або отримання даних на сокеті: https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-shutdown
+    iResult = shutdown(ClientSocket, SD_SEND); 
     if (iResult == SOCKET_ERROR) {
-        cout << "shutdown failed with error: " << WSAGetLastError() << "\n";
-        cout << "oops, disconnection (shutdown) gave an error ((\n";
         closesocket(ClientSocket);
         WSACleanup();
         return 9;
@@ -205,7 +135,6 @@ int main()
         cout << "server process stops working! to new launches! :)\n";
     }
 
-    // вивільнення памяті
     closesocket(ClientSocket);
     WSACleanup();
 
